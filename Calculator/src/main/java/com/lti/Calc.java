b764555936f1417b5f62976682eb25cbbeca7db6
Shivi Gupta <shivi.gupta1101@gmail.com>
2020-12-21 14:58:42 +0530

    added sub

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,10 +5,14 @@ public class Calc {
 	public static int add(int n1,int n2) {
 		return n1+n2;
 	}
+	public static int sub(int n1,int n2) {
+		return n1-n2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(add(10,10));
+		System.out.println("sum " +add(10,10));
+		System.out.println("Difference "+sub(10,10));
 	}
 
 }
