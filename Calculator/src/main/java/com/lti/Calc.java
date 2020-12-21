5834f511226cc735d363f9b3116dddfef9831523
Shivi Gupta <shivi.gupta1101@gmail.com>
2020-12-21 15:09:38 +0530

    prod added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -8,11 +8,15 @@ public class Calc {
 	public static int sub(int n1,int n2) {
 		return n1-n2;
 	}
+	public static int prod(int n1,int n2) {
+		return n1*n2;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("sum " +add(10,10));
+		System.out.println("Sum " +add(10,10));
 		System.out.println("Difference "+sub(10,10));
+		System.out.println("Product " +prod(10,10));
 	}
 
 }
